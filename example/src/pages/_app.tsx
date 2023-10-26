c870a8042c33b37370a4b982fd3efa087589a674
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-10-26 18:59:25 +0200

    feat: Added routeMatcher prop

--- example/src/pages/_app.tsx
+++ example/src/pages/_app.tsx
@@ -2,11 +2,15 @@ import { NextjsCurtain } from '@oak-digital/nextjs-curtain';
 import type { AppProps } from 'next/app';
 import '@oak-digital/react-curtain/dist/style.css';
 import '../styles/util.css';
+import { useCallback } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+    const routeMatcher = useCallback((route: string) => {
+        return route.startsWith('/about');
+    }, []);
     return (
         <div style={{ position: 'relative' }}>
-            <NextjsCurtain duration={10} curtainClassName="bg-dark z-10" position="absolute">
+            <NextjsCurtain routeMatcher={routeMatcher} duration={10} curtainClassName="bg-dark z-10" position="absolute">
                 <Component {...pageProps} />
             </NextjsCurtain>
         </div>
--- src/components/NextjsCurtain/NextjsCurtain.tsx
+++ src/components/NextjsCurtain/NextjsCurtain.tsx
@@ -1,38 +1,49 @@
 import { Curtain, CurtainProps } from '@oak-digital/react-curtain';
 import { useRouter } from 'next/router.js';
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
-export type NextjsCurtainProps = Omit<CurtainProps, 'visible'>;
+export type NextjsCurtainProps = Omit<CurtainProps, 'visible'> & {
+    /**
+     * A function to determine whether the curtain should be visible for the given route
+     * It is recommended to return false for shallow routes
+     * Please use useCallback or useMemo to memoize the function
+     * By default it is a function that will return true if the route is not shallow
+     */
+    routeMatcher?: (route: string, options: { shallow?: boolean }) => boolean;
+};
 
-export const NextjsCurtain: FC<NextjsCurtainProps> = (props) => {
+const defaultRouteMatcher: Exclude<NextjsCurtainProps['routeMatcher'], undefined> = (_url, { shallow }) => {
+    if (shallow) {
+        return false;
+    }
+    return true;
+};
+
+export const NextjsCurtain: FC<NextjsCurtainProps> = ({ routeMatcher = defaultRouteMatcher, ...curtainProps }) => {
     const [visible, setVisible] = useState(false);
 
     const router = useRouter();
 
     useEffect(() => {
         const handleRouteChangeStart = (
-            _url: string,
-            {
-                shallow,
-            }: {
+            url: string,
+            options: {
                 shallow?: boolean;
             }
         ) => {
-            if (shallow) {
+            if (!routeMatcher(url, options)) {
                 return;
             }
             setVisible(true);
         };
 
         const handleRouteChangeComplete = (
-            _url: string,
-            {
-                shallow,
-            }: {
+            url: string,
+            options: {
                 shallow?: boolean;
             }
         ) => {
-            if (shallow) {
+            if (!routeMatcher(url, options)) {
                 return;
             }
             setVisible(false);
@@ -47,7 +58,7 @@ export const NextjsCurtain: FC<NextjsCurtainProps> = (props) => {
             router.events.off('routeChangeComplete', handleRouteChangeComplete);
             router.events.off('routeChangeError', handleRouteChangeComplete);
         };
-    }, []);
+    }, [routeMatcher]);
 
-    return <Curtain {...props} visible={visible} />;
+    return <Curtain {...curtainProps} visible={visible} />;
 };
